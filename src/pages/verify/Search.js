71286f6e768c9c6475a91b432472229913dc33e5
Sandeep Kumar <satyasandeep786@gmail.com>
2022-08-06 01:49:51 +0530

    feat: Integrate VerifyCertificate method in UI

--- src/pages/verify/Search.js
+++ src/pages/verify/Search.js
@@ -1,6 +1,10 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 
-export default function Example() {
+const Search = (props) => {
+  const { search } = props;
+  const [certId, setCertId] = useState("");
+
   return (
     <div className="p-5">
       <div className="mt-1 relative flex items-center">
@@ -8,17 +12,21 @@ export default function Example() {
           type="text"
           name="search"
           id="search"
-          placeholder="Search your favorite creators"
+          value={certId}
+          onChange={(e) => {
+            setCertId(e.target.value);
+          }}
+          placeholder="Enter the Cert Id to verify details"
           className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
         />
-        <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-          <kbd className="inline-flex items-center   rounded px-2 text-sm font-sans font-medium text-gray-400">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </kbd>
-        </div>
+        <button
+          onClick={() => search(certId)}
+          className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Search
+        </button>
       </div>
     </div>
   );
-}
+};
+
+export default Search;
