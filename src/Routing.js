76a84779a2252b2ca97668192c578964f5422b4f
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2022-08-05 22:58:30 +0530

    feat(ui): Add verify page

--- src/Routing.js
+++ src/Routing.js
@@ -8,8 +8,11 @@ import Footer from "./layouts/Footer";
 import Home from "./pages/home/Home";
 /** Pages */
 import Profile from "./pages/profile/Profile";
-import IssuedCertificate from "./pages/issued-certificates/IssuedCertificate";
-import IssueCertificate from "./pages/issue-certificates/IssueCertificates";
+import Verify from "./pages/verify/Verify";
+
+import IssuedCertificates from "./pages/issued-certificates/IssuedCertificates";
+import IssueNewCertificate from "./pages/issue-new-certificate/IssueNewCertificate";
+
 import SelfCertificates from "./pages/self-certificates/SelfCertificates";
 import SharedCertificates from "./pages/shared-certificates/SharedCertificates";
 
@@ -20,18 +23,17 @@ function Routing() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/" element={<Home />} />
+        
           <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/verify" element={<Home />} />
-          {/* Issuer Routes */}
+          <Route exact path="/verify" element={<Verify />} />
 
-          <Route path="/certificates/issuer/issued" element={<IssuedCertificate />} />
-          <Route path="/certificates/issuer/issuenew" element={<IssueCertificate />} />
+          {/* Issuer Routes */}
+          <Route path="/issuer/issued" element={<IssuedCertificates />} />
+          <Route path="/issuer/issue-new" element={<IssueNewCertificate />} />
 
           {/* User Routes */}
-
-          <Route path="/certificates/user/self" element={<SelfCertificates />} />
-          <Route path="/certificates/user/shared" element={<SharedCertificates />} />
+          <Route path="/user/self" element={<SelfCertificates />} />
+          <Route path="/user/shared" element={<SharedCertificates />} />
         </Routes>
         <Footer />
       </BrowserRouter>
